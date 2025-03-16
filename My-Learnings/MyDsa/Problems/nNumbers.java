a955dd7fc06d3293c4ccc04875e8ea577e19b735
Eniyavan-602 <eniyavan822@gmail.com>
2025-03-16 15:53:10 +0530

    Removed submodule and added MyDsa/Problems folder and files

---
+++ My-Learnings/MyDsa/Problems/nNumbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class nNumbers { // Corrected class name (capital 'O' and 'N')
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+        int n = scan.nextInt();
+        int res = sum(n); // Corrected call to the sum method
+        System.out.println("sumofnumbers: " + res);
+        scan.close(); // Close the Scanner
+    }
+
+    public static int sum(int n) {
+        int sum = (n * (n + 1)) / 2;
+        return sum;
+    }
+}
