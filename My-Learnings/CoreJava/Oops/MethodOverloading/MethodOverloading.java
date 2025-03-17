06d61dcf79d8ef494aa201ede7b55b9953bb0351
Eniyavan-602 <eniyavan822@gmail.com>
2025-03-17 13:10:07 +0530

    Method Overloading

--- My-Learnings/CoreJava/Oops/MethodOverloading/MethodOverloading.java
+++ My-Learnings/CoreJava/Oops/MethodOverloading/MethodOverloading.java
@@ -1,6 +1,34 @@
 /*
  * Method Overloading / Polymorphism / compile Time Polymorphism / static Binding / Early Binding
- */
+    Steps for Method Overloading :
+    1.Name
+    2.Number of parameter
+    3.Type of Parameter
+    4.Implicit Type Casting
+    (Ambiguity error)[calc.add('A','B')
+    class Calculator{
+    void add(int a, char b){
+    }
+    void add(char a, int b){
+    }
+    }
+    ]
+    Can you over load Main Method ?
+    Yes , Main Method Can be overloaded
+    PSVM(){
+        s.opln("2";)
+        }
+    PSVM(int a){
+    s.o.pln("3");
+    }
+    psvm(String[]args)//Main Method {
+    s.o.pln("1");
+    }
+    o/p:
+    1
+    2
+    3
+ */ 
 import java.util.Scanner;
 
 class Calculator{
