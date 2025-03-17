953892772a0a7c4581ee44fb569f232b8923a76c
Eniyavan-602 <eniyavan822@gmail.com>
2025-03-17 12:33:31 +0530

    Core Java Concepts

---
+++ My-Learnings/CoreJava/Oops/MethodOverloading/MethodOverloading.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+class Calculator{
+    void add(){
+        System.out.println(100+200);
+    }
+    void add(int a,int b){
+        System.out.println(a+b);
+    }
+    void add(int a, float b){
+        System.out.println(a+b);
+    }
+    void  add(int a, float b, float c){
+        System.out.println(a+b+c);
+    }
+    void add(int a, int b, int c){
+        System.out.println(a+b+c);
+    }
+
+}
+public class MethodOverloading {
+    public static void main(String[] args) {
+     Scanner scan =   new Scanner(System.in);
+       Calculator calc = new Calculator();
+        calc.add();
+        calc.add(20,40);
+        calc.add(20,30.3f);
+        calc.add(20,30.3f,40);
+        calc.add(20,40,50);
+    }
+
+}
